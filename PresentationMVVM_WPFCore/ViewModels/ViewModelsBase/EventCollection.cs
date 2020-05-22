5f9c7273091ef377b8989ec66eddd49437bb9fc4
Charlotte Beck <Charlotte.Beck@stag.technofuturtic.education>
2020-05-22 10:00:31 +0200

    Màj 22/05/2020

---
+++ PresentationMVVM_WPFCore/ViewModels/ViewModelsBase/EventCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PresentationMVVM_WPFCore.ViewModels.ViewModelsBase
+{
+    public class EventCollection
+    {
+
+    }
+}
